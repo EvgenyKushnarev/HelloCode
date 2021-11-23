d6420953b9140e3e6a31566073593be51e455921
evgeny <evgeniu_k99@mail.ru>
2021-11-23 19:22:48 +0300

    Task04

---
+++ Independent_work/Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = new Random().Next(1, 1000);
+int B = new Random().Next(1, 1000);
+int C = new Random().Next(1, 1000);
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine(C);
+int max = A;
+if(A > max) max = A;
+if(B > max) max = B;
+if(C > max) max = C;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
