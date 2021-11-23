5b4adf8daca81c61b36fbf95d075a4490db3df4f
evgeny <evgeniu_k99@mail.ru>
2021-11-23 18:13:43 +0300

    Task01

---
+++ Independent_work/Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = new Random().Next(1, 1000);
+Console.WriteLine(A);
+int B = new Random().Next(1,1000); 
+Console.WriteLine(B);
+int result = A / B;
+
+if(result == B)
+{
+    Console.WriteLine("Первое число является квадратом второго числа!!!");
+}
+else
+{
+    Console.WriteLine("Первое число, к сожалению, не является квадратом второго числа");
+}
