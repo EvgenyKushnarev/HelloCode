7f4d78774a047308393ba1977880f99184037ed4
evgeny <evgeniu_k99@mail.ru>
2021-11-23 18:30:12 +0300

    Task02

---
+++ Independent_work/Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = new Random().Next(1, 1000);
+Console.WriteLine(A);
+int B = new Random().Next(1,1000); 
+Console.WriteLine(B);
+
+
+if(A > B)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
